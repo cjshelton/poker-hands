f7b4abfd9f55ff20e80e69d34007bf5bfd500ece
Chris Shelton <27889958+cjshelton@users.noreply.github.com>
2021-12-31 15:04:39 +0000

    Add header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Image from 'next/image';
 import styles from '../styles/Home.module.scss';
+import aces from '../public/images/aces.png';
 
 const Home: NextPage = () => {
     return (
@@ -14,15 +16,25 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <header className={styles['home__header']}>
-                <h1>Poker Hands</h1>
-            </header>
+            <div className={styles['home__inner']}>
+                <header className={styles['home__header']}>
+                    <div className={styles['home__aces-container']}>
+                        <Image
+                            src={aces}
+                            alt="Four playing cards fanned out each one being an Ace of a suit"
+                            layout="responsive"
+                            placeholder="blur"
+                        />
+                    </div>
+                    <h1 className={styles['home__title']}>Poker Hands</h1>
+                </header>
 
-            <main className={styles['home__content']}>
-                <h1>Body</h1>
-            </main>
+                <main className={styles['home__content']}>
+                    <h1>Body</h1>
+                </main>
 
-            <footer className={styles['home__footer']}>Footer</footer>
+                <footer className={styles['home__footer']}>Footer</footer>
+            </div>
         </div>
     );
 };
