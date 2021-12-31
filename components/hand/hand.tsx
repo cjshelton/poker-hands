87f29ce7f9614d700363b3a1e6a3ecf1b7321562
Chris Shelton <27889958+cjshelton@users.noreply.github.com>
2021-12-31 17:54:31 +0000

    Update type

--- components/hand/hand.tsx
+++ components/hand/hand.tsx
@@ -9,7 +9,10 @@ export default function Hand({
 }: {
     name: string;
     description?: string;
-    cards: { imgSrc: any; alt: string }[];
+    cards: {
+        imgSrc: StaticImageData;
+        alt: string;
+    }[];
 }) {
     return (
         <section className={styles['hand']}>
